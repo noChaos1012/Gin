68836d5d109bbdb1630c8c9e319bb45134112eb8
noChaos1012 <waschild@163.com>
2020-08-18 21:19:26 +0800

    go mod init && gin init

--- gin.go
+++ gin.go
@@ -1,7 +1,19 @@
 package gin
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
-func main(){
-	fmt.println("hello gin")
+func main() {
+	engine := gin.Default()
+
+	engine.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	engine.Run("31000")
+
+	fmt.Println("hello gin")
 }
